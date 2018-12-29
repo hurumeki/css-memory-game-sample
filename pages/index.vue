9304c2b7179297d742354664188b748bab1263f9
hurumeki <hurumeki1987@gmail.com>
2018-12-29 09:54:52 +0900

    カードをめくる

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,54 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        css-memory-game-sample
-      </h1>
-      <h2 class="subtitle">
-        My marvelous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div>
+    <input
+      id="check"
+      type="checkbox">
+    <div class="card">
+      <div class="front">
+        <div class="face">
+          Card
+        </div>
       </div>
-    </div>
-  </section>
+      <div class="back" />
+    </div>    
+  </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<style lang="scss">
+body {
+  padding: 10px;
+}
 
-export default {
-  components: {
-    Logo
+.card {
+  border: 1px solid black;
+  height: 100px;
+  text-align: center;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+  position: relative;
+  width: 60px;
+
+  .front {
+    background: white;
+    transform: rotateY(180deg);
+    position: absolute;
+    height: 100%;
+    width: 100%;
+
+    .face {
+      line-height: 100px;
+    }
+  }
+
+  .back {
+    background: black;
+    position: absolute;
+    height: 100%;
+    width: 100%;
   }
 }
-</script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+:checked + .card {
+  transform: rotateY(180deg);
 }
 </style>
