d582f4ea9db761a1fff580c6e18ad33abd01d093
hurumeki <hurumeki1987@gmail.com>
2018-12-29 11:33:35 +0900

    複数のカードを配置

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,65 @@
 <template>
   <div>
-    <input
-      id="check"
+    <input 
+      v-for="c in cardCount" 
+      :key="c" 
+      :id="`check-${c}`"
       type="checkbox">
-    <div class="card">
-      <label for="check"/>
-      <div class="front">
-        <div class="face">
-          Card
+
+    <div class="cards">
+      <div 
+        v-for="c in cardCount"
+        :key="c"
+        :class="['card', `c-${c}`]">
+        <label :for="`check-${c}`"/>
+        <div class="front">
+          <div class="face">
+            {{ mark[c%markCount-1] }}
+          </div>
         </div>
-      </div>
-      <div class="back" />
-    </div>    
+        <div class="back" />
+      </div>    
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  components: {},
+  data: function() {
+    return {
+      mark: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    }
+  },
+  computed: {
+    markCount: function() {
+      return this.mark.length
+    },
+    cardCount: function() {
+      return this.markCount * 2
+    }
+  }
+}
+</script>
+
 <style lang="scss">
+$mark-count: 8;
+$card-count: $mark-count * 2;
+
 body {
   padding: 10px;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  width: calc((60px + 20px) * 4);
 }
 
 .card {
   border: 1px solid black;
   height: 100px;
+  margin: 10px;
   text-align: center;
   transition: transform 1s;
   transform-style: preserve-3d;
@@ -59,11 +96,13 @@ body {
   }
 }
 
-:checked + .card {
-  transform: rotateY(180deg);
+@for $c from 1 through $card-count {
+  #check-#{$c}:checked ~ .cards .card.c-#{$c} {
+    transform: rotateY(180deg);
 
-  label {
-    transform: translateZ(-1px);
+    label {
+      transform: translateZ(-1px);
+    }
   }
 }
 </style>
